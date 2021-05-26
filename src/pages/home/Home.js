c22a11f340b3a2c78a8f0c95d1b2937129b2c6f5
AnaVitoriaFreitasUSP <anavitoria_gouvea@usp.br>
2021-05-26 19:11:17 -0300

    alterando loop de requisiçao

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,33 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import '../vendas/Vendas.css'
 
 const Home = () => {
-   const [dados, setDados] = useState([]);
+  const [appState, setAppState] = useState([]);
 
+  useEffect(() => {
     fetch(`https://projeto-stone-api.herokuapp.com/produtos/${localStorage.getItem('id')}`)
     .then((response) => response.json())
-    .then(data => setDados(data))
+    .then(data => {
+      setAppState(data);
+    })
     .catch(err=>console.log(err));
 
-   
-   return(
-     <div className="containerProdutos">
-    <h1>Home</h1>
-    <h2>Listar produtos</h2>
-    <div className="gridFotos">
-    {dados.map(produto => (
-      <div key={produto.categoria}>
-        <h2>
-          {produto.titulo}
-        </h2>
+  }, [setAppState]);
+
+
+  return(
+    <div className="containerProdutos">
+      <h1>Home</h1>
+      <h2>Listar produtos</h2>
+      <div className="gridFotos">
+        {appState.map(produto => (
+          <div key={produto.categoria}>
+            <h2>
+              {produto.titulo}
+            </h2>
         <img src={produto.link_img} alt=""/>
       </div>
     ))}
   </div>
   </div>
-   )
+  )
 
-   
+
 }
 
 export default Home
