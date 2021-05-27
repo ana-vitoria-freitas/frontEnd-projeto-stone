5652c6c9eba7bf5e1d9e1afb6ad6654d782a9bb9
developer <anavitoria_gouvea@usp.br>
2021-05-27 16:02:12 -0300

    paginacao produto feita

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,35 @@
 import React, {useState, useEffect} from 'react'
+// import Pagination from  '../../components/pagination/pagination';
 import '../vendas/Vendas.css'
+
 
 const Home = () => {
   const [appState, setAppState] = useState([]);
+  const [page, setPage] = useState(1);
+  const [qtProdutos, setQtProdutos] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+
+  const previousPage = () =>{
+    if(page !== 1){
+      setPage(page - 1);
+    }
+  }
+
+  useEffect(() =>{
+    fetch(`https://projeto-stone-api.herokuapp.com/produtos/${localStorage.getItem('id')}`)
+    .then((response) => response.json())
+    .then(data => {
+      setQtProdutos(data[0].count);
+    })
+
+  },[qtProdutos]);
 
   useEffect(() => {
-    fetch(`https://projeto-stone-api.herokuapp.com/produtos/${localStorage.getItem('id')}`,{
+    fetch(`https://projeto-stone-api.herokuapp.com/produtos/${localStorage.getItem('id')}/${page}/12`,{
       headers:{
         'Accept': 'application/json',
         'Content-Type': 'application/json',
@@ -18,25 +42,38 @@ const Home = () => {
     })
     .catch(err=>console.log(err));
 
-  }, [setAppState]);
+    setIsLoading(false);
+
+  }, [page]);
+
+  return (
+    <div>
+      <h1>Produtos</h1>
+      {isLoading && <p>Loading data from the server...</p>}
 
 
-  return(
-    <div className="containerProdutos">
-      <h1>Home</h1>
-      <h2>Listar produtos</h2>
-      <div className="gridFotos">
-        {appState.map(produto => (
-          <div key={produto.categoria}>
-            <h2>
-              {produto.titulo}
-            </h2>
-        <img src={produto.link_img} alt=""/>
-      </div>
-    ))}
-  </div>
-  </div>
-  )
+      {appState.map((c, index) => (
+        <div key={index}>
+          <p>{c.titulo}</p>
+        </div>
+      ))}
+      {
+        appState.length !== 0 && page !== 1 && (
+          <button onClick={previousPage}>Anterior</button>
+        )
+      }
+      {
+        page !== (Math.ceil(qtProdutos/12)) && (
+          <button>{page}</button>
+        )
+
+      }
+      {appState.length !== 0 && page !== (Math.ceil(qtProdutos/12)) && (
+        <button onClick={nextPage}>Próxima</button>
+      )}
+    </div>
+  );
+
 
 
 }
